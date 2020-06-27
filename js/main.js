c28368a0c6563a0bbea3e0230976b462375ef0e9
Нина Волчек <mysha76@mail.ru>
2020-06-27 22:39:55 +0300

    Внесены изменения по Babel

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-const mySwiper = new Swiper('.swiper-container', {
+"use strict";
+var mySwiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
   navigation: {
-    nextEl: '.arrow',
+    nextEl: '.arrow'
   },
   breakpoints: {
     560: {
-      slidesPerView: 2,
+      slidesPerView: 2
     }
   }
-
 });
 
 
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.header');
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
 
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener('click', function () {
   menuButton.classList.toggle('menu-button-active');
   menu.classList.toggle('header-active');
-})
+});
 
 
 

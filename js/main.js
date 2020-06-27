a516eb19ed3de3873d258604dcb28173fad02e2d
Нина Волчек <mysha76@mail.ru>
2020-06-27 20:53:07 +0300

    Добавлен просмотр трейлера на ютуб в плейере. Добавлена анимация

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   navigation: {
     nextEl: '.arrow',
   },
+  breakpoints: {
+    560: {
+      slidesPerView: 2,
+    },
+  }
+
 });
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})

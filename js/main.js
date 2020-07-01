945ce73234eae82c08c8827fad0c33ec141c3820
Нина Волчек <mysha76@mail.ru>
2020-07-01 10:13:14 +0300

    Внесены изменения для кроссбраузерности

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 "use strict";
-var mySwiper = new Swiper('.swiper-container', {
-  loop: true,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.arrow'
-  },
-  breakpoints: {
-    560: {
-      slidesPerView: 2
+
+document.addEventListener('DOMContentLoaded', function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.arrow'
+    },
+    breakpoints: {
+      560: {
+        slidesPerView: 2
+      }
     }
-  }
+  });
+  var menuButton = document.querySelector('.menu-button');
+  var menu = document.querySelector('.header');
+  menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+  });
 });
-
-
-var menuButton = document.querySelector('.menu-button');
-var menu = document.querySelector('.header');
-
-menuButton.addEventListener('click', function () {
-  menuButton.classList.toggle('menu-button-active');
-  menu.classList.toggle('header-active');
-});
-
-
 
